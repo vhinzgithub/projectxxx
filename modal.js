77a07031e7c82201316fd82b93eb44b140e4a13c
vhinzgithub <50746982+vhinzgithub@users.noreply.github.com>
2019-06-07 22:11:26 +0800

    Add files via upload

--- modal.js
+++ modal.js
@@ -11,23 +11,39 @@ $(document).ready(function(){
 	var span = document.getElementsByClassName("login-close")[0];
 
 	// When the user clicks the button, open the modal 
-	btn.onclick = function() {
-	  modal.style.display = "block";
+
+	// btn.onclick = function() {
+	//   modal.style.display = "block";
 
 	  
-	}
+	// }
 
-	btn2.onclick = function() {
-	  modal.style.display = "block";
+	$('#login-btn').click(function(){
+		modal.style.display = "block";
+	})
+
+
+
+
+	// btn2.onclick = function() {
+	//   modal.style.display = "block";
 
 	
-	}
+	// }
+
+	$('#login-btn2').click(function(){
+		modal.style.display = "block";
+	});
 
 	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() {
-	  modal.style.display = "none";
-	}
+	// span.onclick = function() {
+	//   modal.style.display = "none";
+	// }
 
+	$('.login-close').click(function(){
+		 modal.style.display = "none";
+	});
+	
 
 	window.onclick = function(event) {
 	  if (event.target == modal) {
